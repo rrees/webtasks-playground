b78b7bddf883f456588dbc19c0aa0db7962dd640
Robert Rees <shudderfix@gmail.com>
2017-03-08 23:30:03 +0000

    Experiments with having another route

--- express.js
+++ express.js
@@ -7,4 +7,8 @@ app.get('/', function (req, res) {
   res.send('<h1>Hello World!</h1>');
 });
 
+app.get('/hello/:name', function(request, response) {
+  response.send("<h1>Hello " + request.params.name +"!</h1>");
+});
+
 module.exports = Webtask.fromExpress(app);
